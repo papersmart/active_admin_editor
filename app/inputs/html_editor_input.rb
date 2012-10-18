6c894b177f79d59675bdb468cd53bb2026502c0d
Simon Stemplinger <simon@simon-stemplinger.com>
2012-10-18 21:10:02 +0200

    remove all image asset stuff

--- app/inputs/html_editor_input.rb
+++ app/inputs/html_editor_input.rb
@@ -7,7 +7,6 @@ class HtmlEditorInput < Formtastic::Inputs::TextInput
       <a data-wysihtml5-command="bold" title="Bold"><span>Bold</span></a>
       <a data-wysihtml5-command="italic" title="Italic"><span>Italic</span></a>
       <a data-wysihtml5-command="createLink" title="Link"><span>Link</span></a>
-      <a data-wysihtml5-command="insertImage" class="insertImage" title="Image"><span>Image</span></a>
       <a data-wysihtml5-command="insertUnorderedList" title="Unordered list"><span>Bulleted List</span></a>
       <a data-wysihtml5-command="insertOrderedList" title="Ordered list"><span>Numbered List</span></a>
       <a data-wysihtml5-command="formatBlock" data-wysihtml5-command-value="h1">h1</a>
--- lib/active_admin/editor/rails/engine.rb
+++ lib/active_admin/editor/rails/engine.rb
@@ -3,9 +3,9 @@ module ActiveAdmin
     module Rails
       class Engine < ::Rails::Engine
         engine_name 'active_admin_editor'
-        config.after_initialize do
-          require 'active_admin/editor/admin/assets'
-        end
+        # config.after_initialize do
+          # require 'active_admin/editor/admin/assets'
+        # end
       end
     end
   end
